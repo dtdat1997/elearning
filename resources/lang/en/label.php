c664194b76d33ae772d6f67ba679d196f4e59d49
dtdat1997 <duongtiendat1997@gmail.com>
2019-01-29 14:51:17 +0700

    Front end detail course

--- resources/lang/en/label.php
+++ resources/lang/en/label.php
@@ -11,6 +11,7 @@ return [
     'password' => 'Mật khẩu',
     'remember_me' => 'Ghi nhớ đăng nhập',
     'forgot_password' => 'Quên mật khẩu',
+    'home' => 'Home',
 
     //Home page
     'course' => 'Course',
@@ -81,6 +82,32 @@ return [
     'by' => 'by',
     'related_to' => 'related_to',
     'this_post_is_visible_to_everyone.' => 'This post is visible to everyone.',
+
+    //Detail course
+    'self_paced' => 'Self-Paced',
+    'enroll_now' => 'Enroll Now',
+    'i_would_like_to_receive_email' => 'I would like to receive email from Edx and learn about other offerings related to this course.',
+    'about_this_course' => 'About this course',
+    'what_you_ll_learn' => "What you'll learn",
+    'view_course_syllabus' => 'View Course Syllabus',
+    'meet_the_instructor' => 'Meet the instructor',
+    'length' => 'Length',
+    'effort' => 'Effort',
+    'price' => 'Price',
+    'institution' => 'Institution',
+    'level' => 'Level',
+    'video_transcripts' => 'Video Transcripts',
+    'share_this_course_with_a_friend' => 'Share this course with a friend',
+    'pursue_a_verified_certificate' => 'Pursue a Verified Certificate to highlight the knowledge and skills you gain',
+    'official_and_verified' => 'Official and Verified',
+    'easily_shareable' => 'Easily Shareable',
+    'proven_motivator' => 'Proven Motivator',
+    'support_our_mission' => 'Support our Mission',
+    'receive_an_instructor-signed' => "Receive an instructor-signed certificate with the institution's logo to verify your achievement",
+    'add_the_certificate' => 'Add the certificate to your CV or resume, or post it directly on LinkedIn',
+    'give_yourself_an_additional' => 'Give yourself an additional incentive to complete the course',
+    'edx_a_non-profit' => 'EdX, a non-profit, relies on verified certificates to help fund free education for everyone globally',
+ 
     'lang' => [
         'en' => 'English',
         'vi' => 'Tiếng Việt',
