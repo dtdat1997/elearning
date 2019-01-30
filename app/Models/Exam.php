41952b181de4c4c9d9bd60e6f43c071e585afa71
dtdat1997 <duongtiendat1997@gmail.com>
2019-01-30 13:42:07 +0700

    Model Test

---
+++ app/Models/Exam.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exam extends Model
+{
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'user_id', 
+        'component_id', 
+        'spent_time', 
+        'duration', 
+        'status', 
+        'score',
+    ];
+
+    public function components()
+    {
+        return $this->belongsTo('App\Models\Component');
+    }
+
+    public function questions()
+    {
+        return $this->belongsToMany('App\Models\Question', 'exam_question', 'exam_id', 'question_id')->withPivot('id');
+    }
+}
